99648c09e2524437ba71f9da8d0f7683d84471d7
Youssef Eladl <youssefeladl2005@gmail.com>
2024-12-05 16:21:06 +0200

    o

---
+++ MS3DB/Pages/Login.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MS3DB
+{
+	public partial class Login : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void login (object sender, EventArgs e)
+		{
+			String connStr = WebConfigurationManager.ConnectionStrings["MyDatabaseConnection"].ToString();
+
+			SqlConnection conn = new SqlConnection(connStr);
+
+			int id = Int16.Parse(Username.Text);
+
+			String password = Password.Text;
+
+			SqlCommand loginproc = new SqlCommand("userlogin", conn);
+			loginproc.CommandType = CommandType.StoredProcedure;
+			loginproc.Parameters.Add(new SqlParameter("@id",id));
+			loginproc.Parameters.Add(new SqlParameter("@password", password));
+
+			SqlParameter success = loginproc.Parameters.Add("@success", SqlDbType.Int);
+			SqlParameter type = loginproc.Parameters.Add("@type", SqlDbType.Int);
+
+			success.Direction = ParameterDirection.Output;
+			type.Direction = ParameterDirection.Output;
+
+			conn.Open();
+			loginproc.ExecuteNonQuery();
+			conn.Close();
+
+			if (success.Value.ToString() == "1")
+			{
+				Response.Write("Hello");
+			}
+
+
+		}
+	}
+}
